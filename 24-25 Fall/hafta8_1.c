b643d2eca9c69c43b93ff447c4d2ca4ce5035385
Batuhan Karadağ <52932122+batukar@users.noreply.github.com>
2024-12-20 13:11:52 +0300

    week 8, edited

---
+++ 24-25 Fall/hafta8_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int x = 0; // global değişken
+
+int input(); // fonksiyon prototipi
+
+int main(){
+    int num1 = input();
+    int num2 = input();
+
+    printf("Girilen sayi = %d\n", num1);
+    printf("Girilen sayi = %d\n", num2);
+}
+
+int input(){
+    printf("Sayi girin: \n");
+    scanf("%d", &x);
+
+    return x;
+}
+
